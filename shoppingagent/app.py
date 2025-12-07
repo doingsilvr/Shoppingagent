f45ddce5e7f07290c40196ba0bbbc4a57fff6902
doingsilvr <eunvin0119@gmail.com>
2025-12-07 19:51:10 +0900

    Update app.py

--- shoppingagent/app.py
+++ shoppingagent/app.py
@@ -1897,7 +1897,7 @@ def main_chat_interface():
     
         # summary 외 단계에서는 안내 문구만
         if st.session_state.stage != "summary":
-            st.info("수정하실 기준이 있으면 아래 입력창에서 말씀해주세요. 😊 (*종종 에이전트가 출력오류로 동일한 질문을 던질 수 있습니다. 그럴 땐 '추천해줘'를 입력해주세요!)
+            st.info("수정하실 기준이 있으면 아래 입력창에서 말씀해주세요. 😊 또한 **종종 에이전트가 출력오류로 동일한 질문을 던질 수 있습니다. 그럴 땐 '추천해줘'를 입력해주세요!")
 
         # ------------------------------------------------
         # 입력폼
@@ -1947,6 +1947,7 @@ if st.session_state.page == "context_setting":
     context_setting_page()
 else:
     main_chat_interface()
+
 
 
 
